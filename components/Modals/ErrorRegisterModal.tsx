e2b39cfaa2f0efce518c77ab990a390976133444
Dano Gonzalez <21393109@utcancun.edu.mx>
2024-11-28 02:01:43 -0500

    Ajustes de modal error y studes

---
+++ components/Modals/ErrorRegisterModal.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+
+interface ErrorModalProps {
+  visible: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+const ErrorRegisterModal = ({ visible, onClose, message }: ErrorModalProps) => {
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}>
+      <View style={styles.overlay}>
+        <View style={styles.modalContent}>
+          <Image
+            source={require("../../assets/ModalImages/CancelGeneral.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.title}>{message}</Text>
+          <TouchableOpacity style={styles.closeButton} onPress={onClose}>
+            <Text style={styles.buttonText}>Cerrar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 20,
+    alignItems: "center",
+    width: "80%",
+    paddingVertical: 30,
+  },
+  icon: {
+    width: 80,
+    height: 80,
+    marginBottom: 20,
+    resizeMode: "contain",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginBottom: 20,
+    textAlign: "center",
+    color: "#000",
+  },
+  closeButton: {
+    backgroundColor: "#0078FF",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
+
+export default ErrorRegisterModal;
